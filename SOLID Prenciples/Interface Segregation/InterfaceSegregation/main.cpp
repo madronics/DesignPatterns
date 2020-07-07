a34deaa393102a524c9ead6850c567baa52624fd
DESKTOP-LITSON2\EU1 <oguzhan.altun@eurobotik.com>
2020-07-07 15:43:56 +0300

    Interface Segregation Finish Commit

--- SOLID Prenciples/Interface Segregation/InterfaceSegregation/main.cpp
+++ SOLID Prenciples/Interface Segregation/InterfaceSegregation/main.cpp
@@ -1,9 +1,70 @@
-#include <iostream>
+#include <vector>
+struct Document;
 
-using namespace std;
+//struct IMachine
+//{
+//  virtual void print(Document& doc) = 0;
+//  virtual void fax(Document& doc) = 0;
+//  virtual void scan(Document& doc) = 0;
+//};
+//
+//struct MFP : IMachine
+//{
+//  void print(Document& doc) override;
+//  void fax(Document& doc) override;
+//  void scan(Document& doc) override;
+//};
 
-int main()
+// 1. Recompile
+// 2. Client does not need this
+// 3. Forcing implementors to implement too much
+
+struct IPrinter
 {
-    cout << "Hello World!" << endl;
+  virtual void print(Document& doc) = 0;
+};
+
+struct IScanner
+{
+  virtual void scan(Document& doc) = 0;
+};
+
+struct Printer : IPrinter
+{
+  void print(Document& doc) override;
+};
+
+struct Scanner : IScanner
+{
+  void scan(Document& doc) override;
+};
+
+struct IMachine: IPrinter, IScanner
+{
+};
+
+struct Machine : IMachine
+{
+  IPrinter& printer;
+  IScanner& scanner;
+
+  Machine(IPrinter& printer, IScanner& scanner)
+    : printer{printer},
+      scanner{scanner}
+  {
+  }
+
+  void print(Document& doc) override {
+    printer.print(doc);
+  }
+  void scan(Document& doc) override;
+};
+
+// IPrinter --> Printer
+// everything --> Machine
+
+int main(int argc, char *argv[])
+{
     return 0;
 }
+
